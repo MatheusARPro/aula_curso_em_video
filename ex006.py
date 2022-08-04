c23e77c2b3617c06a5946b061dd5bc8aa11c37d1
theus <theus@DESKTOP-RSGSQDO.local>
2022-08-04 01:35:04 -0300

    Dobro, triplo e raiz

---
+++ ex006.py
@@ -0,0 +1,5 @@
+n1 = float(input('Digite um número: '))
+n2 = n1 * 2
+n3 = n1 * 3
+n4 = n1 ** (1/2)
+print('O dobro de {} é igual a {}, \nO triplo de {} é igual a {} \nA raiz quadrada de {} é igual a {:.3f}'.format(n1, n2, n1,  n3, n1, n4))
