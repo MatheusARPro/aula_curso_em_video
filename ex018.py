f8edccf9f28bb58357f502e2d70d97a2bd33f0e3
theus <theus@DESKTOP-RSGSQDO.local>
2022-08-04 01:52:15 -0300

    Seno, cosseno e tanggente

---
+++ ex018.py
@@ -0,0 +1,3 @@
+from math import(cos, tan, sin)
+ang = float(input('Qual o valor do angulo: '))
+print('Do angulo de  {} graus \nO cosseno do angulo é {:.3f} \nO seno é {:.3f} \nA tangente é {:.3f}'.format(ang, cos(ang), sin(ang), tan(ang)))
