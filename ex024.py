3e2c16ae372dd7d3aa44156b8c3220b16062e952
theus <theus@DESKTOP-RSGSQDO.local>
2022-08-08 12:46:36 -0300

    'SANTO' IN CITY

---
+++ ex024.py
@@ -0,0 +1,6 @@
+city = input('Qual sua cidade? ').upper()
+
+if 'SANTO' in city :
+    print('O nome da sua cidade possui a palavra "Santo".')
+else :
+    print('O nome da sua cidade não possui a palavra "Santo"')
