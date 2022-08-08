287a684a11230534a289842af8180a669fcdf948
theus <theus@DESKTOP-RSGSQDO.local>
2022-08-08 07:50:17 -0300

    Sorteando ordem da lista

--- ex020.py
+++ ex020.py
@@ -7,4 +7,4 @@ a3 = input('Terceiro aluno: ')
 a4 = input('Quarto aluno: ')
 lista = [a1, a2, a3, a4]
 shuffle(lista)
-print(f'A ordem de apresentação será  {lista}')
+print(f'A ordem de apresentação será \n {lista}')
