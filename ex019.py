a1120341e17bb2559a98ed7121f48eddeb653cea
theus <theus@DESKTOP-RSGSQDO.local>
2022-08-08 07:49:54 -0300

    Sorteando um na lista

--- ex019.py
+++ ex019.py
@@ -3,5 +3,5 @@ aluno1 = input('Primeiro aluno: ')
 aluno2 = input('Segundo aluno: ')
 aluno3 = input('Terceiro aluno: ')
 aluno4 = input('Quarto aluno: ')
-lista = aluno1, aluno2, aluno3, aluno4
+lista = [aluno1, aluno2, aluno3, aluno4]
 print('O aluno escolhido foi o/a {}.'.format(random.choice(lista)))
