aee51a61c37bc8d1669b7df3933a165365d6a230
theus <theus@DESKTOP-RSGSQDO.local>
2022-08-08 21:18:27 -0300

    Analisador de nome

--- ex022.py
+++ ex022.py
@@ -1,6 +1,18 @@
-name = input('Qual seu nome completo ? ')
-#nome2 = nome.split()
+
+name = str( input('Qual seu nome completo ? '))
+
 print(f"""Seu nome em maiusculas : {name.upper()}
 Seu nome em minusculas : {name.lower()}
 Seu nome completo sem espaços tem {len(name.replace(' ',''))} letras
 Seu primeiro nome tem {len(name.split()[0])} letras""")
+
+
+'''
+name = str( input('Qual seu nome completo ? ')).strip()
+
+print(f"""Seu nome em maiusculas : {name.upper()}
+Seu nome em minusculas : {name.lower()}
+Seu nome completo sem espaços tem {len(name) - name.count(' ')} letras
+Seu primeiro nome tem {name.find(' ')} letras""")
+'''
+
