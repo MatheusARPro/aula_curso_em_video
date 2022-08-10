d231c8b6a2974272fbae4008a59e1666e9fc8807
theus <theus@DESKTOP-RSGSQDO.local>
2022-08-10 10:38:35 -0300

    Primeiro e Ultimo nome.

--- ex027.py
+++ ex027.py
@@ -1,5 +1,5 @@
-name = input('Digite seu nome: ')
+name = input('Digite seu nome: ').strip()
 
-print(f'''Nome completo: {name}
-Primeiro nome: {name.split()[0]}
-Ultimo nome: {name.split()[len(name.split())-1]}''')
+print(f'''Nome completo: {name.title()}
+Primeiro nome: {name.split()[0].title()}
+Ultimo nome: {name.split()[len(name.split())-1].title()}''')
