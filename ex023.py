a4c3a0bdcdaadc10ecd374ecbb490b4c656a7bf1
theus <theus@DESKTOP-RSGSQDO.local>
2022-08-10 03:44:44 -0300

    Quebrando um numero

--- ex023.py
+++ ex023.py
@@ -1,13 +1,20 @@
+#forma string(nao funciona corretamente.)
+'''
+num = int(input('Digite um numero de 0 a 9999: '))
+n = str(num)
+
+print(f"""Analisando o numero {num}
+A unidade é {n[3]}
+A dezena é {n[2]}
+A centena é {n[1]}
+A milhar é {n[0]}""")
+'''
+
+#forma matematica(funcional.)
 num = int(input('Digite um numero de 0 a 9999: '))
 
-#print(f'''Do numero {num}
-#A unidade é {num[3]}
-#A dezena é {num[2]}
-#A centena é {num[1]}
-#A milhar é {num[0]}''')
-
-print(f"""Do número {num}
-A unidade é {num // 1 % 10 }
-A dezena é {num // 10 % 10}
-A centena é {num // 100 % 10}
-A milhar é {num // 1000 % 10}""")
+print(f"""Analisando o numero {num}
+A unidade é : {num // 1 % 10 }
+A dezena é : {num // 10 % 10}
+A centena é : {num // 100 % 10}
+A milhar é : {num // 1000 % 10}""")
