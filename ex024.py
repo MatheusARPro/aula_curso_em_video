9bf7444601397a849675ea4e8b2d00acc6db90f0
theus <theus@DESKTOP-RSGSQDO.local>
2022-08-10 10:17:19 -0300

    Santo no  nome da cidade.

--- ex024.py
+++ ex024.py
@@ -1,6 +1,16 @@
-city = input('Qual sua cidade? ').upper()
+'''city = input('Qual sua cidade? ').upper().strip()
 
 if 'SANTO' in city :
     print('O nome da sua cidade possui a palavra "Santo".')
 else :
     print('O nome da sua cidade não possui a palavra "Santo"')
+'''
+
+
+'''c = input('Qual a sua cidade? ').upper().strip()
+
+print('Existe Santo no nome da cidade?','SANTO' in c)
+'''
+
+cid = str(input('Em qu cidade voce nasceu? ')).strip()
+print(cid[:5].upper() == 'SANTO')
