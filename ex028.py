94fc3c54302606e459c3f37305e93fa16efca54e
theus <theus@DESKTOP-RSGSQDO.local>
2022-08-11 03:27:29 -0300

    Adivinhador de numeros!

---
+++ ex028.py
@@ -0,0 +1,12 @@
+from random import randint
+
+
+num_sort = randint(0, 5)
+escolha = int(input('Qual numero de 0 a 5 o programa esta "pensando"? '))
+
+if escolha == num_sort:
+    print('PARABÉNS!!!!Você venceu!')
+else:
+    print('Que pena, você PERDEU!!!')
+    print(f'Eu estava pensando no numero {num_sort}!!')
+    
