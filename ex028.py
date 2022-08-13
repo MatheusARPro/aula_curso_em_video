4ef91c86890ec49882383cb787bd1f7b0a33e037
theus <theus@DESKTOP-RSGSQDO.local>
2022-08-13 02:53:49 -0300

    Advinihador de numeros

--- ex028.py
+++ ex028.py
@@ -1,11 +1,16 @@
 from random import randint
-
+from time import sleep
 
 num_sort = randint(0, 5)
-escolha = int(input('Qual numero de 0 a 5 o programa esta "pensando"? '))
-
+print('#'* 55)
+print('Estou pensando em um número!')
+escolha = int(input('Qual número de 0 a 5 eu estou "pensando"? '))
+print('PROCESSANDO...')
+sleep(3)
+print('#'* 55)
 if escolha == num_sort:
     print('PARABÉNS!!!!Você venceu!')
 else:
     print('Que pena, você PERDEU!!!')
-    print(f'Eu estava pensando no numero {num_sort}!!')
+    print(f'Eu pensei no {num_sort} e não no número {escolha}!!')
+print('#'* 55)
