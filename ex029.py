349dd30e802aba91dd45bba13a226e708f5ed11e
theus <theus@DESKTOP-RSGSQDO.local>
2022-08-13 03:08:17 -0300

    limite de velocidade

--- ex029.py
+++ ex029.py
@@ -4,3 +4,4 @@ if vel > 80:
     print('Você ultrapassou o limite de velocidade, e foi multado.')
     multa = (vel - 80) * 7
     print(f'O valor da multa será de R${multa:.2f}')
+print('Dirija com segurança!!')
