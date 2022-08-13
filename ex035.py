8fe273db32f77724fa480c64ea2d8ebee01b346e
theus <theus@DESKTOP-RSGSQDO.local>
2022-08-13 11:16:52 -0300

    Possibilidade de Triangulo

--- ex035.py
+++ ex035.py
@@ -1,4 +1,4 @@
-r1 = float(input('Medida da primeira reta: '))
+'''r1 = float(input('Medida da primeira reta: '))
 r2 = float(input('Medida da segunda reta: '))
 r3 = float(input('Medida da terceira reta: '))
 
@@ -13,4 +13,16 @@ p3 = t3 > r1
 if p1 and p2 and p3:
     print('As retas podem forma um triângulo!')
 else:
-    print('As retas não podem formar um triângulo!')
+    print('As retas não podem formar um triângulo!')'''
+
+print('-=-'*20)
+print(('Analisador de Triângulos'))
+print('-=-'*20)
+r1 = float(input('Medida da primeira reta: '))
+r2 = float(input('Medida da segunda reta: '))
+r3 = float(input('Medida da terceira reta: '))
+
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r2 + r1:
+    print('Os segmentos acima PODEM FORMAR triândulo!')
+else:
+    print('O segmentos acima NÃO PODEM FORMAR triângulo!')
