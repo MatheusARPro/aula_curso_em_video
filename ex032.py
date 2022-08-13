b0e16485ed34bf2385d7c2845f4db201fd970df7
theus <theus@DESKTOP-RSGSQDO.local>
2022-08-13 04:11:09 -0300

    Ano Bissexto.

--- ex032.py
+++ ex032.py
@@ -1,4 +1,4 @@
-from calendar import isleap
+'''from calendar import isleap
 
 ano = int(input('Digite um ano: '))
 
@@ -6,3 +6,16 @@ if isleap(ano) == True:
     print(f'O ano {ano} é um ano Bissexto!')
 else:
     print(f'O ano {ano} não é um ano bissexto!')
+'''
+
+from datetime import date
+
+
+ano = int(input('Que ano gostaria de analisar? Coloque 0 para o ano atual. '))
+
+if ano == 0:
+    ano = date.today().year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print(f'O ano {ano} é bissexto!')
+else:
+    print(f'O ano {ano} NÃO é bissexto!')
