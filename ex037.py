e19c7ff2b7676a36697915fa9bfc4bab5139b125
theus <theus@DESKTOP-RSGSQDO.local>
2022-08-18 10:56:14 -0300

    BINARIO,OCTAL E HEXADECIMAL

--- ex037.py
+++ ex037.py
@@ -1,14 +1,19 @@
 numero = int(input('Digite um numero: '))
-base = int(input('Qual vai ser a base de conversão?(Digite [1]Binário, [2]Octal, [3]Hexadecimal) '))
+print('''Qual vai ser a base de conversão? 
+[ 1 ]Binário
+[ 2 ]Octal
+[ 3 ]Hexadecimal ''')
 
+base = int(input('Sua escolha: '))
 binario = bin(numero)
 octal = oct(numero)
 hexadecimal = hex(numero)
 
 if base == 1:
-    print(f'O numero {numero}, convertido para binário é: {binario}')
+    print(f'O numero {numero}, convertido para binário é: {binario[2:]}')
 elif base == 2:
-    print(f'O numero {numero}, convertido para octal é: {octal}')
+    print(f'O numero {numero}, convertido para octal é: {octal[2:]}')
 elif base == 3:
-    print(f'O numero {numero}, convertido para hexadecimal é: {hexadecimal}')
-    
+    print(f'O numero {numero}, convertido para hexadecimal é: {hexadecimal[2:]}')
+else:
+    print('OPÇÃO INVÁLIDA.TENTE NOVAMENTE.')
