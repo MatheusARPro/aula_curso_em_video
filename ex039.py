2149efd13f98e0f87057ade21ad82b657e870996
theus <theus@DESKTOP-RSGSQDO.local>
2022-08-18 11:09:24 -0300

    Alistamento Militar

--- ex039.py
+++ ex039.py
@@ -7,9 +7,14 @@ idade = ano_atual - nascimento
 
 if idade < 18:
     tempo_restante = 18 - idade
+    ano = tempo_restante + ano_atual
     print(f'Ainda ira se alistar, faltam apenas {tempo_restante} anos.')
+    print(f'Seu alistamento sera em {ano} ')
 elif idade == 18:
     print('Você esta na idade cereta para o alistamento, ALISTE-SE!!!')
 elif idade > 18:
     tempo_restante = idade - 18
+    ano = ano_atual - tempo_restante
     print(f'O alistamento venceu, ja passou {tempo_restante} anos do prazo.')
+    print(f'Seu alistamento foi em {ano}')
+    
