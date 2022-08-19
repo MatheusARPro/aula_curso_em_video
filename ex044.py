a3f2502b9a30b42a494e2e352862942d8889fb0b
theus <theus@DESKTOP-RSGSQDO.local>
2022-08-19 10:08:18 -0300

    Total a pagar

--- ex044.py
+++ ex044.py
@@ -10,19 +10,26 @@ if pag == 1:
     valor_pag = preço - ((preço / 100 ) * 10)
     print(f'''DESCONTO: 10%
 JUROS: 0%
-VALOR À PAGAR: {valor_pag:.2f}''')
+VALOR À PAGAR: R${valor_pag:.2f}''')
 elif pag == 2:
     valor_pag = preço -((preço / 100) * 5)
     print(f'''DESCONTO: 5%
 JUROS: 0%
-VALOR À PAGAR: {valor_pag:.2f}''')
+VALOR À PAGAR: R${valor_pag:.2f}''')
 elif pag == 3:
+    parcela = preço /  2
     print(f'''DESCONTO: 0%
 JUROS: 0%
-VALOR À PAGAR: {preço}''')
+VALOR DA PARCELA: R${parcela}
+VALOR À PAGAR: R${preço}''')
 elif pag == 4:
+    par = int(input('NÚMERO DE PARCELAS: '))
     valor_pag = preço + ((preço / 100) * 20)
+    parcela = valor_pag / par
     print(f'''DESCONTO: %
 JUROS: 20%
-VALOR À PAGAR: {valor_pag:.2f}''')
+VALOR DA PARCELA: R${parcela}
+VALOR À PAGAR: R${valor_pag:.2f}''')
+else:
+    print('OPÇÃO INVALIDA.TENTE NOVAMENTE.')
 print('#'*25)
