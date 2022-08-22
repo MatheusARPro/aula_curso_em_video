6b575606a08c2c1a40808caff00b7d6218106cf1
theus <theus@DESKTOP-RSGSQDO.local>
2022-08-22 13:15:08 -0300

    Soma pares

--- ex050.py
+++ ex050.py
@@ -1,5 +1,7 @@
-pt = int(input('Qual o primeiro termo da PA: '))
-razao = int(input('Qual a razão da PA: '))
-r = pt + (razao * 10)
-for c in range(pt, r, razao):
-    print(c)
+soma = 0
+for c in range(0, 6):
+    numero = input('Digite um valor: ')
+    if numero.isnumeric():
+        if int(numero) % 2 == 0:
+            soma += int(numero)
+print(f'A soma dos numeros pares é igual a {soma}')
