e17b5311ec00a655d7b617c6d8921e656e30b5b6
theus <theus@DESKTOP-RSGSQDO.local>
2022-09-17 12:31:37 -0300

    Soma multiplos de 3

--- ex048.py
+++ ex048.py
@@ -1,7 +1,9 @@
 soma = 0
+cont = 0
 for c in range(1, 501):
     if c % 2 == 1:
         i = c
         if i % 3 == 0:
             soma = soma + i
-print(f'A soma dos numeros impares, divisiveis por 3 no intervalo de 1 a 500 é {soma}')
+            cont = cont + 1
+print(f'A soma dos {cont} numeros impares, divisiveis por 3 no intervalo de 1 a 500 é {soma}')
