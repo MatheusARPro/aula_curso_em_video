487c68d4e45747f2ab01bb595bb5bb1167207062
theus <theus@DESKTOP-RSGSQDO.local>
2022-09-18 12:50:12 -0300

    detector de peso

--- ex055.py
+++ ex055.py
@@ -1,12 +1,16 @@
-m_peso = 0
-
+ma_peso = 0
+me_peso = 0
+nome1 = 0
+nome2 = 0
 for c in range(0, 5):
-    print('++' * 20)
-    print('  DETECTOR DE PESO  ')
-    print(f'''O mais pesado foi: 
-    Peso: {m_peso}Kg''')
     print('++' * 20)
     nome = input('Nome: ')
     peso = float(input('Peso: '))
-    if peso >= m_peso:
-        m_peso = peso
+    if peso >= ma_peso:
+        ma_peso = peso
+        nome1 = nome
+    else:
+        if peso <= me_peso:
+            me_peso = peso
+            nome2 = nome
+print(f'O mais pesado foi {nome1} com {ma_peso}Kg \nO mais leve foi {nome2} com {me_peso}Kg')
