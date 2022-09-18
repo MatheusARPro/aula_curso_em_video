9fb11cf65b1196548ed379d73fc19d7d7eed1bdb
theus <theus@DESKTOP-RSGSQDO.local>
2022-09-17 22:29:18 -0300

    Tabuada 2.0

--- ex049.py
+++ ex049.py
@@ -1,6 +1,6 @@
 numero = int(input('Digite o numero para ver a tabuada: '))
 print('#' * 15)
 for multiplicador in range(1, 11):
-    resultado = multiplicador * numero
-    print(f'{multiplicador:2} X {numero} = {resultado}')
+    print(f'{numero:2} X {multiplicador:2} = {numero*multiplicador}')
 print('#' * 15)
+2
