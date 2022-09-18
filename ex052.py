fa0c8101bc763a2555f9a40f18d727ed0ff03d9b
theus <theus@DESKTOP-RSGSQDO.local>
2022-09-18 12:18:22 -0300

    Numero Primo

--- ex052.py
+++ ex052.py
@@ -1,4 +1,4 @@
-continua = True
+'''continua = True
 while continua:
     numero = int(input('Digite um numero: '))
     if numero <= 1 and numero % 2 == 0:
@@ -16,4 +16,20 @@ while continua:
             print('Este número não é primo.')
     continuar = int(input("Quer continuar? Digite 0 para sim e 1 para não: "))
     if continuar == 1:
-        continua = False
+        continua = False'''
+
+
+num = int(input('Digite um numero: '))
+tot = 0
+for c in range(1, num + 1):
+    if num % c == 0:
+        print('\033[33m', end='')
+        tot += 1
+    else:
+        print('\033[031m', end='')
+    print(f'{c} ',  end='')
+print(f'\n\033[mO numero {num} foi divisivel {tot} vezes')
+if tot == 2:
+    print('E por isso ele é PRIMO!')
+else:
+    print('e por isso ele NÃO É PRIMO!')
