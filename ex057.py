465bd1dd2ff3365c599c701e2c4201fa98aca46a
theus <theus@DESKTOP-E815H03.local>
2022-12-12 04:39:06 -0300

    Update ex057.py

--- ex057.py
+++ ex057.py
@@ -1,9 +1,19 @@
-sexo = '0'
+'''sexo = '0'
 while sexo != 'F' and sexo != 'M':
-    sexo = str(input('Digite o sexo[M/F]: ')).upper()
+    sexo = str(input('Digite o sexo[M/F]: ')).upper().strip()[0]
     if sexo != 'F' and sexo != 'M':
         print('SEXO INVÁLIDO!')
 if sexo == 'M':
     print('Você digitou o sexo "MASCULINO"! ')
 elif sexo == 'F':
-    print('Você digitou o sexo "FEMININO"!')
+    print('Você digitou o sexo "FEMININO"!')'''
+
+
+sexo = str(input('Informe o Sexo: [M/F] ')).strip().upper()[0]
+while sexo not in 'FfMm':
+    sexo = str(input('Invalido. Por favor, informe o sexo: [M/F] ')).strip().upper()[0]
+if sexo == 'M':
+    sexo = 'Masculino'
+elif sexo == 'F':
+    sexo = 'Feminino'
+print(f'Sexo informado: {sexo}')
