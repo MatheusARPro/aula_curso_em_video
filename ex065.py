bc554e61756384b1581d287c49b1be5d2b5d3cd8
theus <theus@DESKTOP-E815H03.local>
2022-12-14 05:17:36 -0300

    Maior, menor e média

--- ex065.py
+++ ex065.py
@@ -1,4 +1,4 @@
-soma = 0
+'''soma = 0
 c = 0
 maior = 0
 menor = 0
@@ -21,6 +21,25 @@ while continua != 1:
         print ('Opçao invalida!!!')
         continua = int(input('Gostaria de continuar?Digite 0 Para sim e 1 para Nao. '))
 
-media = soma / c .__float__()
-print(f'''A media entre os numeros digitados e igual a {media}
-O maior numero foi {maior} e o menor numero foi {menor}''')
+media = soma / c .__float__()'''
+#print(f'''A media entre os numeros digitados e igual a {media}
+#O maior numero foi {maior} e o menor numero foi {menor}''')
+
+
+resp = 's'
+media = quant = soma = maior = menor = 0
+while resp in 'Ss':
+    num = int(input('Digite um numero: '))
+    soma += num
+    quant += 1
+    if quant == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        elif num < menor:
+            menor = num
+    resp = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+media = soma / quant
+print(f'Você digitou {quant} numeros, e a média foi {media:.2f}.')
+print(f'O maior numero foi {maior}, e o menor numero foi {menor}')
