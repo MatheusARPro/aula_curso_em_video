868394fab974340bbeaa0bef1809a7d077b5c77f
theus <theus@DESKTOP-E815H03.local>
2022-12-14 04:13:30 -0300

    Pa em While

--- ex061.py
+++ ex061.py
@@ -2,7 +2,9 @@ pt = int(input('Qual o primeiro termo da PA: '))
 razao = int(input('Qual a razão da PA: '))
 c = 0
 while c < 10:
-    print(pt)
+    print(f' {pt}', end='')
+    print(',' if c < 9 else '...', end='')
     c = c + 1
     r = pt + razao
     pt = r
+print('FIM')
