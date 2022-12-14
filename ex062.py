f08745900a7092da719ec552ce2903cbc335f745
theus <theus@DESKTOP-E815H03.local>
2022-12-14 04:35:09 -0300

    PA em While com continuaçao

--- ex062.py
+++ ex062.py
@@ -1,21 +1,44 @@
-pt = int(input('Qual o primeiro termo da PA: '))
+'''pt = int(input('Qual o primeiro termo da PA: '))
 razao = int(input('Qual a razão da PA: '))
 c = 0
 while c < 10:
-    print(pt)
+    print(f' {pt}', end='')
+    print(',' if c < 9 else '...', end='')
     c = c + 1
     r = pt + razao
     pt = r
 n = 1
 while n != 0:
-    n = int(input('''Gostaria de ver mais termos?
-Digite a quantidade ou Digite "0" para finalizar:'''))
+    n = int(input('''#Gostaria de ver mais termos?
+#Digite a quantidade ou Digite "0" para finalizar:'''))
+'''
     if n == 0:
         print('Finalizado!!')
     else:
         n1 = c + n
         while c < n1:
-            print(pt)
+            print(f' {pt}', end='')
+            print(',' if c < n1 else '...', end='')
             c = c + 1
             r = pt + razao
             pt = r
+'''
+
+
+
+pt = int(input('Qual o primeiro termo da PA: '))
+razao = int(input('Qual a razão da PA: '))
+termo = pt
+c = 0
+total = 0
+mais = 10
+while mais != 0:
+    total += mais
+    while c < total:
+        print(f' {termo}', end='')
+        print(',' if c < 9 else '...', end='')
+        termo += razao
+        c = c + 1
+    print('PAUSA')
+    mais = int(input('Quantos termos você quer mostrar a mais?  '))
+print(f'Progressão finalizada com {total} termos mostrados.')
