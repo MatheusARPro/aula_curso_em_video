846f9c8dd066fdd933d908f2cfb9643d70fc68cf
theus <theus@DESKTOP-E815H03.local>
2022-12-14 04:46:29 -0300

    Sequencia de Fibonacci

--- ex063.py
+++ ex063.py
@@ -1,12 +1,12 @@
 cs = int(input('Quntos termos gostaria de ver? '))
 pt = 0
 st = 1
-print(f'''{pt}
-{st} ''')
+print(f'{pt} - {st} - ', end='')
 c = 2
 while c != cs:
     ts = pt + st
-    print(ts)
+    print(f'{ts} - ', end='')
     c = c + 1
     pt = st
     st = ts
+print('FIM.')
