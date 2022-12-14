c58bc5d9749ea623a5aaec1ce9eb35acfa0c869f
theus <theus@DESKTOP-E815H03.local>
2022-12-14 03:52:04 -0300

    Factorial

--- ex060.py
+++ ex060.py
@@ -1,4 +1,4 @@
-n = int(input('Digite o numero que deseja calcular o fatorial: '))
+'''n = int(input('Digite o numero que deseja calcular o fatorial: '))
 while n < 0:
     print('NUMERO INVALIDO,TENTE NOVAMENTE!!!')
     n = int(input('Digite o numero que deseja calcular o fatorial: '))
@@ -10,4 +10,28 @@ else:
     while f > 1:
         f -= 1
         fd *= f
-print(f'{n}! = {fd}')
+print(f'{n}! = {fd}')'''
+
+
+'''n = int(input('Digite o numero que deseja calcular o fatorial: '))
+c = n
+f = 1
+print(f'Calculando {n}! = ', end='')
+while c > 0:
+    print(f'{c} ', end='')
+    print(' X ' if c > 1 else ' = ', end='' )
+    f *= c
+    c -= 1
+print(f'{f}')'''
+
+
+from  math import factorial
+n = int(input('Digite o numero que deseja calcular o fatorial: '))
+c = n
+print(f'Calculando {n}! = ', end='')
+while c > 0:
+    print(f'{c} ', end='')
+    print(' X ' if c > 1 else ' = ', end='' )
+    c -= 1
+print(factorial(n))
+
