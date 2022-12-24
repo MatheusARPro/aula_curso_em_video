efca6b7a3d9449dfd24a4d223d75ba2b2d723540
theus <theus@DESKTOP-E815H03.local>
2022-12-23 22:24:26 -0300

    Tabuada 3.0

--- ex067.py
+++ ex067.py
@@ -1,13 +1,11 @@
 c = nt = r = 0
 while True:
     nt = int(input('Digite o numero para ver a tabuada: '))
-    print ('¥'*50)
+    print ('¥'*40)
     for c in range(1, 11):
         r = c * nt
         print(f'{nt:2} X {c:2} = {r:2}')
-    print ('¥'*50) 
-    continuar = input('Gostaria de continuar[S/N]: ') .upper().strip()[0]
-    if continuar not in 'Ss':
+    print ('¥'*40)
+    if nt < 0:
         break
-print ('¥'*50)
-print ('Execução Finalizada!') 
+print ('Execução Finalizada!')
