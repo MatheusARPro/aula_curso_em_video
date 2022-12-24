c01e548d24a960bbc373c3409b82ff844e3db676
theus <theus@DESKTOP-E815H03.local>
2022-12-23 23:03:59 -0300

    Tabuada 3.0

--- ex067.py
+++ ex067.py
@@ -2,10 +2,10 @@ c = nt = r = 0
 while True:
     nt = int(input('Digite o numero para ver a tabuada: '))
     print ('¥'*40)
+    if nt < 0:
+        break
     for c in range(1, 11):
         r = c * nt
         print(f'{nt:2} X {c:2} = {r:2}')
     print ('¥'*40)
-    if nt < 0:
-        break
 print ('Execução Finalizada!')
