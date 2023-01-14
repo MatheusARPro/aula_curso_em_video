72db11977005cb9231046e3711d7a6ce96418637
theus <theus@DESKTOP-E815H03.local>
2023-01-14 05:10:50 -0300

    Analisador de Compras

---
+++ ex070.py
@@ -0,0 +1,24 @@
+valor_total = menor_valor = c = maior_mil = 0
+produto_barato = 'hjh'
+while True:
+    produto = str(input('Produto: '))
+    valor = float(input('Valor: R$ '))
+    valor_total += valor
+    c += 1
+    if valor > 1000:
+        maior_mil += 1
+    if c == 1:
+        menor_valor = valor
+    if valor < menor_valor:
+        menor_valor = valor
+        produto_barato = produto
+    while True:
+        continuar = str(input('Quer continuar?[S/N] ')).upper().strip()[0]
+        if 'S' in continuar or 'N' in continuar:
+            break
+    if continuar in 'N':
+        break
+
+print(f'Total gasto: R${valor_total:.2f}')
+print(f'Produtos acima de R$1.000,00: {maior_mil}')
+print(f'Produto mais barato: {produto_barato}')
