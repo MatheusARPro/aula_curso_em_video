3bb430288cb9268bbbe4604c5ed123ffb0d365f9
theus <theus@DESKTOP-E815H03.local>
2023-02-05 12:12:13 -0300

    PAR OU IMPAR 2.0

--- ex068.py
+++ ex068.py
@@ -1,4 +1,4 @@
-from random import randint
+'''from random import randint
 
 resultado = int
 print('-*-' *20)
@@ -36,4 +36,36 @@ while True:
             print('Você perdeu!!!')
             print('---' * 20)
             break
-    print('O jogo acabou!!!')
+    print('O jogo acabou!!!')'''
+
+
+
+#soluçao do Guananbara
+#colocando pq achei interessante
+from random import randint
+v = 0
+while True:
+    jogador = int(input('Diga um valor: '))
+    computador = randint( 0, 10)
+    total = jogador + computador
+    tipo = ' '
+    while tipo not in 'PI':
+        tipo = str(input('PAR ou IMPAR? [P/I]: ')).strip().upper()[0]
+    print(f'Você jogou {jogador} e o computador {computador}. Total de {total} ' , end='')
+    print('DEU PAR' if total % 2 == 0 else 'DEU IMPAR')
+    if tipo == 'P':
+        if total % 2 == 0 :
+            print('Você VENCEU!')
+            v += 1
+        else:
+            print('Você PERDEU!')
+            break
+    elif tipo == 'I':
+        if total % 2 == 1:
+            print('Você VENCEU!')
+            v += 1
+        else:
+            print('Você PERDEU!')
+            break
+    print('Vamos jogar novamente...')
+print(f'GAME OVER!!!Você venceu {v} vez(es).')
