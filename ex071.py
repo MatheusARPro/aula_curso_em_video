4357fc30a768db95e454d9a0369eed12483c3a6d
theus <theus@DESKTOP-E815H03.local>
2023-02-07 14:09:18 -0300

    MARBank

--- ex071.py
+++ ex071.py
@@ -1,6 +1,6 @@
-print('-_-_-' * 5)
-print('       MARBank   ')
-print('-_-_-' * 5)
+'''  print('=' * 30)
+print('{:^30}'.format('MARBank'))
+print('=' * 30)
 saque = int(input('valor do saque: R$ '))
 
 cinquenta = vinte = dez = um = 0
@@ -36,11 +36,40 @@ while True:
             break
     break
 
-print('-_-_-' * 5)
-print(f'''Cédulas de 50 => {cinquenta}
+print('=' * 30)
+print(f'Cédulas de 50 => {cinquenta}
 Cédulas de 20 => {vinte}
 Cédulas de 10 => {dez}
-Cédulas de 1 => {um}''')
-print('-_-_-' * 5)
-print('Obrigado por usar o MARbank!')
-print('-_-_-' * 5)
+Cédulas de 1 => {um}')
+print('=' * 30)
+print('{:^30}'.format('Obrigado por usar o MARbank!'))
+print('=' * 30)'''
+
+
+#Resoluçao do Guanabara'
+print('=' * 30)
+print('{:^30}'.format('MARBank'))
+print('=' * 30)
+saque = int(input('valor do saque: R$ '))
+total = saque
+ced = 50
+totced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totced += 1
+    else:
+        if totced > 0:
+            print(f'{totced} de cédulas de R${ced}')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totced = 0
+        if total == 0:
+            break
+print('=' * 30)
+print('{:^30}\n{:^30}'.format('Volte Sempre ao MARBank!','TENHA UM BOM DIA!!'))
+print('=' * 30)
