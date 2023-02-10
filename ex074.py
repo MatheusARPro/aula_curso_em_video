ee33258c0c59eba3907323d93cf66485097a5875
theus <theus@DESKTOP-E815H03.local>
2023-02-10 18:11:00 -0300

    Maior e Menor valor em Tuplas

--- ex074.py
+++ ex074.py
@@ -1,6 +1,6 @@
 from random import randint
 
-numaleatorio = (randint(0,100), randint(0,100), randint(0, 100), randint(0, 100), randint(0,100))
+'''numaleatorio = (randint(0,100), randint(0,100), randint(0, 100), randint(0, 100), randint(0,100))
 print(f'Numeros Gerados: {numaleatorio}')
 menor = 101
 maior = 0
@@ -10,4 +10,12 @@ for num in range(0, len(numaleatorio)):
     if numaleatorio[num] < menor:
         menor = numaleatorio[num]
 print(f'O maior numero gerado foi: {maior}')
-print(f'O menor numero gerado foi: {menor}')
+print(f'O menor numero gerado foi: {menor}')'''
+
+#resoluçao do Guanabara
+numeros = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
+print('Os valores sorteado foram: ', end='')
+for n in numeros:
+    print(f'{n}', end=' ')
+print(f'\nO maior valor sorteado foi: {max(numeros)}')
+print(f'O menor valor sorteado foi; {min(numeros)}')
