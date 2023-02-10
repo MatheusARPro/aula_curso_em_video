8bfbad152b3e56b967b07ad8b1a76b5d8b373865
theus <theus@DESKTOP-E815H03.local>
2023-02-10 17:58:30 -0300

    Brasileirao com Tuplas

--- ex073.py
+++ ex073.py
@@ -1,15 +1,26 @@
 times = ('Palmeiras', 'Internacional', 'Fluminense', 'Corinthians', 'Flamengo', 'Athletico-PR', 'Atlético-MG', 'Fortaleza', 'São Paulo', 'América-MG', 'Botafogo', 'Santos', 'Goiás', 'Bragantino', 'Coritiba', 'Cuiabá', 'Ceará', 'Atlético-GO', 'Avaí', 'Juventude')
-print('#'*105)
-print('{:^105}'.format('Brasileirão'))
-print(f'Os 5 primeiros colocado são: \n{times[0:5]}')
-print('+'*105)
-print(f'Os 4 ultimos colocados são: \n{times[16:]}')
-print('+'*105)
-print(f'Lista Alfabética: \n{sorted(times)}')
-print('+'*105)
+print('#'*80)
+print('{:^80}'.format('Brasileirão'))
+print('#'*80)
+print('Lista em ordem de classificação')
+for t in times:
+    print(t)
+print('+'*80)
+print(f'Os 5 primeiros colocado são:')
+for t in times[:5]:
+    print(t)
+print('+'*80)
+print(f'Os 4 ultimos colocados são: ')
+for t in times[16:]:
+    print(t)
+print('+'*80)
+print(f'Lista Alfabética:')
+for t in sorted(times):
+    print(t)
+print('+'*80)
 if 'Chapecoense' in times:
     posicao = times.index('Chapecoense') + 1
     print(f'O Chapecoense esta na {posicao}ª posição!')
 else:
     print('O Chapecoense não esta entre os 20 primeiros colocados do Brasileirão! ')
-print('#'*105)
+print('#'*80)
