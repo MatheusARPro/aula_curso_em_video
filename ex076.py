cfdd7cd9ff35275378cb52cc1d4ff72883858230
theus <theus@DESKTOP-E815H03.local>
2023-02-10 18:52:32 -0300

    Lista de preços em Tuplas

--- ex076.py
+++ ex076.py
@@ -5,3 +5,23 @@ print('-'*35)
 for c in range(0 , len(produtos), 2):
     print(f'{produtos[c]:-<23}R$ {produtos[c + 1]:8.2f}')
 print('-'*35)
+
+#sugestao do Guanabara
+listagem = ('Lapis', 1.75,
+            'Borracha', 2,
+            'Caderno', 15.90,
+            'Estojo', 25,
+            'Transferidor', 4.20,
+            'Compasso', 9.99,
+            'Mochila', 120.32,
+            'canetas', 22.30,
+            'Livro', 34.90)
+print('-'*40)
+print(f'{"LISTAGEM DE PREÇOS":^40}')
+print('-'*40)
+for pos in range(0, len(listagem)):
+    if pos % 2 == 0:
+        print(f'{listagem[pos]:.<30}', end='')
+    else:
+        print(f'R${listagem[pos]:>7.2f}')
+print('-'*40)
