2a325cf777c4e976d0107ce1c248c5df9a70e440
theus <theus@DESKTOP-E815H03.local>
2023-02-11 20:01:16 -0300

    Maior e Menor em Listas

---
+++ ex078.py
@@ -0,0 +1,18 @@
+valores = list()
+maior = 0
+menor = float('inf')
+for cont in range(0, 5):
+    valores.append(int(input(f'Digite um valor na posição {cont}: ')))
+    if valores[cont] > maior:
+        maior = valores[cont]
+    if valores[cont] < menor:
+        menor = valores[cont]
+print(f'os valores digitados foran: {valores}')
+print(f'O maior valor foi o valor {maior} nas posições ',end='')
+for c in range(0, len(valores)):
+    if valores[c] == maior:
+        print(f'{valores.index(maior, c)}...',end='')
+print(f'\nO menor valor foi o valor {menor} nas posições ',end='')
+for c in range(0, len(valores)):
+    if valores[c] == menor:
+        print(f'{valores.index(menor, c)}...',end='')
