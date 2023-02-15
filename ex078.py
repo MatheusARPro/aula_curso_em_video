5b3584cbacd4ef9d4dcce52a60a9bfdf2009c5a3
theus <theus@DESKTOP-E815H03.local>
2023-02-15 02:59:52 -0300

    Maior e menor em Listas

--- ex078.py
+++ ex078.py
@@ -9,10 +9,20 @@ for cont in range(0, 5):
         menor = valores[cont]
 print(f'os valores digitados foran: {valores}')
 print(f'O maior valor foi o valor {maior} nas posições ',end='')
-for c in range(0, len(valores)):
+'''for c in range(0, len(valores)):
     if valores[c] == maior:
-        print(f'{valores.index(maior, c)}...',end='')
+        print(f'{valores.index(maior, c)}...',end='')'''
+#Sugestao do Guanabara
+for i, v in enumerate(valores):
+    if v == maior:
+        print(f'{i}...',end='')
+print()
 print(f'\nO menor valor foi o valor {menor} nas posições ',end='')
-for c in range(0, len(valores)):
+'''for c in range(0, len(valores)):
     if valores[c] == menor:
-        print(f'{valores.index(menor, c)}...',end='')
+        print(f'{valores.index(menor, c)}...',end='')'''
+#sugestao do Guanabara
+for i , v in enumerate(valores):
+    if v == menor:
+        print(f'{i}...', end='')
+print()
