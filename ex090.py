65e44ba5a16b26bdb707aa4aa0d0f59295ed1b1c
theus <theus@DESKTOP-E815H03.local>
2023-06-03 10:11:57 -0300

    Situação do aluno em dicionário.

--- ex090.py
+++ ex090.py
@@ -1,15 +1,31 @@
-aluno = {}
-nome = str(input('Nome: '))
-media = float(input('Média: '))
-if media < 5:
-    situaçao = 'Reprovado'
-elif media >= 5:
-    situaçao = 'Aprovado'
+# aluno = {}
+# nome = str(input('Nome: '))
+# media = float(input('Média: '))
+# if media < 5:
+#     situaçao = 'Reprovado'
+# elif media >= 5:
+#     situaçao = 'Aprovado'
+#
+# aluno['Nome'] = nome
+# aluno['Média'] = media
+# aluno['Situaçao'] = situaçao
+#
+# for key, value in aluno.items():
+#     print(f'{key} = {value}', end='')
+#     print()
 
-aluno['Nome'] = nome
-aluno['Média'] = media
-aluno['Situaçao'] = situaçao
 
-for key, value in aluno.items():
-    print(f'{key} = {value}', end='')
-    print()
+#Solução do guanabara
+
+aluno = dict()
+aluno['nome'] = str(input('Nome: '))
+aluno['média'] = float(input(f'Média de {aluno["nome"]}: '))
+if aluno['média'] >= 7:
+    aluno['situação'] =  'Aprovado'
+elif 5 <= aluno['média'] < 7:
+    aluno['situação'] = 'Recuperação'
+else:
+    aluno['situação'] = 'Reprovado'
+
+for k, v in aluno.items():
+    print(f' - {k} é igual a {v}')
