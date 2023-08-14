cebbb757acabd0096605873393a0ae3eb2558352
theus <theus@DESKTOP-E815H03.local>
2023-08-14 03:54:32 -0300

    Print linha adptavel

---
+++ ex097.py
@@ -0,0 +1,13 @@
+#funções
+def escreva(txt):
+    tamanho_texto = len(txt)
+    print("-" * tamanho_texto)
+    print(txt)
+    print("-" * tamanho_texto)
+
+
+#codigos
+escreva('ola,mundo!')
+escreva('O gato roeu a roupa do rei de Roma!')
+escreva('Em Roma a roupa do rei o gato roeu!')
+escreva('CURSO EM VIDEO')
