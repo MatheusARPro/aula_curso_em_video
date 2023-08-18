b4d642374a9087f5842f1cd003198a720695f3bb
theus <theus@DESKTOP-E815H03.local>
2023-08-18 11:23:14 -0300

    Funçao de maior numero

---
+++ ex099.py
@@ -0,0 +1,26 @@
+#funções
+def linha():
+    print('-=-' * 25)
+
+
+def maior(*num):
+    tamanho = len(num)
+    print('Analisando os valores passados...')
+    for numero in num:
+        print(f'{numero},', end='')
+    print(f'Foram passados no total {tamanho} numeros ao todo.')
+    maior_numero = 0
+    for numero in num:
+        if numero >= maior_numero:
+            maior_numero = numero
+    print(f'O maior numero passado foi o {maior_numero}')
+
+
+#codigo
+linha()
+maior(1, 2, 3, 5, 10)
+linha()
+maior(30, 45, 76, 14, 43, 98, 20)
+linha()
+maior(3, 7, 9, 1, 2, 4, 9, 3)
+linha()
