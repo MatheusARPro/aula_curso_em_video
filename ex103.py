ff590971473e5d76e06ac926aed8583b543706c4
theus <theus@DESKTOP-E815H03.local>
2023-09-08 02:49:41 -0300

    Ficha do jogador

---
+++ ex103.py
@@ -0,0 +1,23 @@
+#funçao
+def ficha(txt='<desconhecido>', gols=0):
+    """
+    ->Mostra a quantidade de Gols de um Jogador!
+    :param txt: Nome do jogador.
+    :param gols: Quantidade de gols.
+    :return: nome com quantidade de gols.
+    """
+    print(f'O jogador {txt} marcou {gols} gols!')
+
+
+#codigo
+nome = str(input('Nome do jogador: '))
+gol = str(input('Quantos gols marcados: '))
+if gol is int:
+    g = int(gol)
+else:
+    g = 0
+if nome == '':
+    ficha(gols=g)
+else:
+    ficha(nome, g)
+help(ficha)
