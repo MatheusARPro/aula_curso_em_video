01dea5eed75067a52bf06bc50ce90eb08e3efa48
theus <theus@DESKTOP-E815H03.local>
2024-06-07 01:23:15 -0300

    site está acessivel ?

--- ex114.py
+++ ex114.py
@@ -3,11 +3,11 @@ import webbrowser
 
 def acessa_site(url):
     try:
-        webbrowser.open     (url)
+        webbrowser.open(url)
     except ConnectionError:
         print(f'O site {url} não está acessível no momento.')
     else:
-        print(f'site {url} está acessível no momento.')
+        print(f'O site {url} está acessível no momento.')
 
 
-acessa_site('pudim.com.br')
+acessa_site('https://pudim.com.br')
